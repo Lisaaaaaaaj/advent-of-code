d1c8f8f1385a266c3bc673d753b552aeb4022e2d
Lisaaaaaaaj <li-sa-u@hotmail.com>
2025-12-11 19:36:51 +0100

    add day 1 and 2

---
+++ src/2025/day 1/day 1 pt1.ts
@@ -0,0 +1,42 @@
+import { realData } from "./input.ts";
+
+const workableData = [...realData.split(/\n/g)]
+	.filter((d) => d.length)
+	.map(data => {
+        const [rot, dist] = data.split(/(L|R)(.+)/g).filter(d =>d.trim().length)
+  
+        return [rot,dist]
+    });
+
+let dialStart = 50;
+let pointsAtZeroTimes = 0;
+
+workableData.forEach(([rot, dist]) => {
+    const workableDist = parseInt(dist,10);
+
+    if (dialStart === 0) {
+        pointsAtZeroTimes+=1;
+    }
+
+    if (rot === 'L') {
+        for (let i = 0; i < workableDist; i++) {
+            if (dialStart === 0) {
+                dialStart = 99;
+            } else { 
+                dialStart -= 1 
+            }
+        }
+    }
+
+    if (rot === 'R') {
+        for (let i = 0; i < workableDist; i++) {
+            if (dialStart === 99) {
+                dialStart = 0;
+            } else { 
+                dialStart += 1; 
+            }
+        }
+    }
+})
+
+console.log(pointsAtZeroTimes)
---
+++ src/2025/day 2/day 2 pt1.ts
@@ -0,0 +1,33 @@
+import { realData } from "./input.ts";
+
+const matchInvalidIds = /\b(\d+)\1{1}\b/g;
+
+const handleIsInvalidId = (id:string) => new RegExp(matchInvalidIds).test(id)
+
+let invalidIdsSum = 0;
+
+const addInvalidIdToSum = (id:string) => invalidIdsSum += parseInt(id,10);
+
+[...realData.split(/\n|,/g)]
+	.filter((d) => d.length)
+	.forEach(data => {
+        const [firstId, lastId] = data.split(/-/g).filter(d =>d.trim().length)
+
+        const firstIdIsInvalid = handleIsInvalidId(firstId)
+        const lastIdIsInvalid = handleIsInvalidId(lastId)
+
+        if (firstIdIsInvalid) {
+            addInvalidIdToSum(firstId)
+        }
+        if (lastIdIsInvalid) {
+            addInvalidIdToSum(lastId)
+        }
+
+        for (let i =parseInt(firstId, 10)+1; i < parseInt(lastId,10); i++) {
+            if (handleIsInvalidId(i.toString())){
+                addInvalidIdToSum(i.toString())
+            }
+        }
+    });
+
+console.log(invalidIdsSum)
