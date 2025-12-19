6ca6e66e760eb871eaf2bcafe85e17eff47f3d48
Lisaaaaaaaj <li-sa-u@hotmail.com>
2025-12-19 11:29:56 +0100

    add day 4

---
+++ src/2025/day 4/day 4 pt2.ts
@@ -0,0 +1,84 @@
+import {realData} from "./input.ts";
+const workableData: string[][] = [...realData].map((d) => d.split(''));
+
+const word = '@';
+const numRows = workableData.length;
+const numCols = workableData[0].length;
+
+const directions = [
+	[0, 1],
+	[1, 0],
+	[1, 1],
+	[1, -1],
+	[0, -1],
+	[-1, 0],
+	[-1, -1],
+	[-1, 1],
+];
+
+const searchForRollsOfPaper = (
+	grid: string[][],
+	word: string,
+	x: number,
+	y: number,
+	deltaX: number,
+	deltaY: number
+) => {
+    const nextX = x + 1 * deltaX;
+    const nextY = y + 1 * deltaY;
+    const notOutOfBounds = nextX < 0 || nextY < 0 || nextX >= numRows || nextY >= numCols;
+
+    if (notOutOfBounds || grid[nextY][nextX] !== word) {
+        return false;
+    }
+
+	return true;
+};
+
+let foundTotalRemovedAccessableRollsOfPaper = 0;
+let search = true;
+
+while(search) {
+	let foundAccessableRollsOfPaper = 0;
+	let foundAccessableRollsOfPaperAt: [number,number][] = [];
+
+	const findAccessableRollsOfPaper = (grid: string[][]) => {
+		for (let x = 0; x < numRows; x++) {
+			for (let y = 0; y < numCols; y++) {
+				let foundRollsOfPaper = 0;
+
+				if (grid[y][x] !== word) {
+					continue;
+				}
+
+				for (const [deltaX, deltaY] of directions) {
+					if (searchForRollsOfPaper(grid, word, x, y, deltaX, deltaY)) {
+						foundRollsOfPaper++;
+					}
+				}
+
+				if (foundRollsOfPaper < 4) {
+					foundAccessableRollsOfPaper++;
+					foundAccessableRollsOfPaperAt.push([y,x])
+				}
+			}
+		}
+	};
+	findAccessableRollsOfPaper(workableData);
+
+	if (foundAccessableRollsOfPaper === 0) {
+		search = false;
+		break;
+	} else {
+		foundTotalRemovedAccessableRollsOfPaper += foundAccessableRollsOfPaper;
+	}
+	const removeAccessableFoundRollsOfPapers = (grid:string[][]) => {
+		foundAccessableRollsOfPaperAt.forEach(roll => {
+			grid[roll[0]][roll[1]] = '.';
+		})
+	}
+	removeAccessableFoundRollsOfPapers(workableData);
+}
+
+console.log(foundTotalRemovedAccessableRollsOfPaper)
+
