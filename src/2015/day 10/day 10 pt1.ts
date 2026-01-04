f7f06aa9546a056ad013b4bf5da8431be96b7296
Lisaaaaaaaj <li-sa-u@hotmail.com>
2026-01-04 17:26:02 +0100

    re-add day 10

---
+++ src/2015/day 10/day 10 pt1.ts
@@ -0,0 +1,29 @@
+import { realData } from './input.ts';
+
+let sequence = realData;
+const playTimes = 40;
+
+const play = (seq: string) => {
+    let nextSequence = "";
+    let i = 0;
+
+    while (i < seq.length) {
+        let count = 1;
+
+        while (i + 1 < seq.length && seq[i] === seq[i + 1]) {
+            count += 1;
+            i += 1;
+        }
+
+        nextSequence += count.toString() + seq[i];
+        i += 1;
+    }
+
+    return nextSequence;
+}
+
+for (let i = 0; i < playTimes; i++) {
+    sequence = play(sequence)
+}
+
+console.log(sequence.length)
